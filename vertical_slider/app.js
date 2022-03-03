fb55c38e7c6891d3d93defc0556a0db8fc623eb2
Muzahidul <bekon28@outlook.com>
2022-03-03 12:01:36 +0600

    color updated

--- vertical_slider/app.js
+++ vertical_slider/app.js
@@ -4,7 +4,7 @@ const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
 const slidesLength = slideRight.querySelectorAll('div').length
-
+ 
 let activeSliderIndex=0;
 slideLeft.style.top=`-${(slidesLength - 1)*100}vh`
 let activeSlideIndex = 0
