cb8b1143be6f83dffd799122bcbd7a9f5905a0e3
Muzahidul <bekon28@outlook.com>
2022-03-08 14:36:28 +0600

    added javascript functionality and background video

--- responsive_video_bg/app.js
+++ responsive_video_bg/app.js
@@ -1,1 +1,42 @@
-const menuBtn=document.querySelector('.menu-btn');
+const menuBtn=document.querySelector('.fa-bars');
+const menuDiv=document.querySelector('.menu-btn');
+const navigation=document.querySelector('.navigation');
+const videoBtn=document.querySelectorAll('.nav-btn');
+const slider=document.querySelectorAll('.video-slide');
+
+const content=document.querySelectorAll('.content');
+
+menuBtn.addEventListener('click',()=>{
+   menuBtn.classList.toggle("fa-xmark");
+   menuDiv.classList.toggle("active");
+   navigation.classList.toggle("active");
+
+});
+
+
+var slideNav=function(manual){
+    videoBtn.forEach((btn)=>{
+        btn.classList.remove("active");
+    });
+
+
+    slider.forEach((btn)=>{
+        btn.classList.remove("active");
+    });
+
+    content.forEach((cont)=>{
+        cont.classList.remove("active");
+    })
+
+
+    videoBtn[manual].classList.add("active");
+    slider[manual].classList.add("active");
+    content[manual].classList.add("active");
+
+}
+
+videoBtn.forEach((btn,i)=>{
+    btn.addEventListener('click',()=>{
+        slideNav(i);
+    })
+})
