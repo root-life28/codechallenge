5f85a0c19de643b534e29f69fe2911d3a7c93741
Muzahidul <bekon28@outlook.com>
2022-03-14 10:08:06 +0600

    added total try

--- memory_game/app.js
+++ memory_game/app.js
@@ -1,58 +1,86 @@
 const cardArray=[
     {
-        name:'fries',
-        img:'images/fries.png'
+        name:'argentina',
+        img:'images/flag-of-Argentina.png'
     },
     {
-        name:'cheeseburger',
-        img:'images/cheeseburger.png'
+        name:'bangladesh',
+        img:'images/flag-of-Bangladesh.png'
     },
     {
-        name:'hotdog',
-        img:'images/hotdog.png'
+        name:'brazil',
+        img:'images/flag-of-Brazil.png'
     },
     {
-        name:'ice-cream',
-        img:'images/ice-cream.png'
+        name:'canada',
+        img:'images/flag-of-Canada.png'
     },
     {
-        name:'milkshake',
-        img:'images/milkshake.png'
+        name:'india',
+        img:'images/flag-of-India.png'
     },
     {
-        name:'pizza',
-        img:'images/pizza.png'
+        name:'singapore',
+        img:'images/flag-of-Singapore.png'
     },
     {
-        name:'fries',
-        img:'images/fries.png'
+        name:'usa',
+        img:'images/flag-of-United-States-of-America.png'
     },
     {
-        name:'cheeseburger',
-        img:'images/cheeseburger.png'
+        name:'malaysia',
+        img:'images/flag-of-Malaysia.png'
     },
     {
-        name:'hotdog',
-        img:'images/hotdog.png'
+        name:'uk',
+        img:'images/flag-of-United-Kingdom.png'
     },
     {
-        name:'ice-cream',
-        img:'images/ice-cream.png'
+        name:'argentina',
+        img:'images/flag-of-Argentina.png'
     },
     {
-        name:'milkshake',
-        img:'images/milkshake.png'
+        name:'bangladesh',
+        img:'images/flag-of-Bangladesh.png'
     },
     {
-        name:'pizza',
-        img:'images/pizza.png'
+        name:'brazil',
+        img:'images/flag-of-Brazil.png'
+    },
+    {
+        name:'canada',
+        img:'images/flag-of-Canada.png'
+    },
+    {
+        name:'india',
+        img:'images/flag-of-India.png'
+    },
+    {
+        name:'singapore',
+        img:'images/flag-of-Singapore.png'
+    },
+    {
+        name:'usa',
+        img:'images/flag-of-United-States-of-America.png'
+    },
+    {
+        name:'malaysia',
+        img:'images/flag-of-Malaysia.png'
+    },
+    {
+        name:'uk',
+        img:'images/flag-of-United-Kingdom.png'
     }
 ];
 
 cardArray.sort(()=>0.5-Math.random());
 
+let totalTry=0;
 const gridDisplay=document.querySelector("#grid");
 const resultDisplay=document.querySelector('#result');
+const totalDisplay=document.querySelector('#tryTotal');
+const winMsg=document.querySelector('.win-msg');
+const reset=document.querySelector('.reset');
 let cardChosen=[];
 let cardChosenIds=[];
 const cardsWon=[];
@@ -60,7 +88,7 @@ function createBoard(){
     for(let i=0;i<cardArray.length;i++)
     {
         const card=document.createElement('img');
-        card.setAttribute('src','images/blank.png');
+        card.setAttribute('src','images/blank.jpeg');
         card.setAttribute('data-id',i);
         card.addEventListener('click',flipCard);
         gridDisplay.appendChild(card);
@@ -72,38 +100,42 @@ createBoard();
 
 function checkMatch()
 {
+    totalTry++;
 
     const optionOneId=cardChosenIds[0];
     const optionTwoId=cardChosenIds[1];
     const cards=document.querySelectorAll('#grid img');
     if(optionOneId===optionTwoId)
     {
-        cards[optionOneId].setAttribute('src','images/blank.png');
-        cards[optionTwoId].setAttribute('src','images/blank.png');
+        cards[optionOneId].setAttribute('src','images/blank.jpeg');
+        cards[optionTwoId].setAttribute('src','images/blank.jpeg');
         alert('You have clicked same image!');
-
+        
     }
     if(cardChosen[0]===cardChosen[1])
     {
-        alert('You found a match');
-        cards[optionOneId].setAttribute('src','images/white.png');
-        cards[optionTwoId].setAttribute('src','images/white.png');
+        // alert('You found a match');
+        // cards[optionOneId].setAttribute('src','images/white.png');
+        // cards[optionTwoId].setAttribute('src','images/white.png');
         cards[optionOneId].removeEventListener('click',flipCard);
         cards[optionTwoId].removeEventListener('click',flipCard);
         cardsWon.push(cardChosen);
     }
     else{
-        cards[optionOneId].setAttribute('src','images/blank.png');
-        cards[optionTwoId].setAttribute('src','images/blank.png');
-        alert('Sorry try again');
+        cards[optionOneId].setAttribute('src','images/blank.jpeg');
+        cards[optionTwoId].setAttribute('src','images/blank.jpeg');
+        // alert('Sorry try again');
     }
     resultDisplay.textContent=cardsWon.length;
     cardChosen=[];
     cardChosenIds=[];
     if(cardsWon.length==cardArray.length/2)
     {
-        resultDisplay.textContent="Congratulation!!! You found them all";
+        winMsg.textContent="Congratulation!!! You found them all";
     }
+
+    totalDisplay.textContent=totalTry;
+
 
    
 }   
@@ -122,3 +154,7 @@ function flipCard(){
     }
 
 }
+
+reset.addEventListener('click',()=>{
+    location.reload();
+})
